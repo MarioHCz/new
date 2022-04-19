f807798eb30aa0b02067d61fe2c25d32d1b50e29
MarioHCz <104018437+MarioHCz@users.noreply.github.com>
2022-04-19 14:33:43 -0300

    update2

---
+++ script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE animals;
+CREATE TABLE cats(
+    catname varchar(30),
+    age INTEGER, 
+    color varchar(30)
+);
+INSERT INTO cats values(
+    'Seraphim', 
+    6,
+    'Yellow'
+);
